d827585310a036c2e28520f7ca032c221872d6ae
Alex Mwaura <alexmwaura36@gmail.com>
2022-08-19 18:14:49 +0300

    commit tree 44f00b37f9adfbdc9a17f41f0b491890b00531eb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        array.select {|array| array.split("").sort === @word.split("").sort}
+
+
+    end
+end
+anagram = Anagram.new("super metro")
+anagram.word
+puts anagram.word
+
